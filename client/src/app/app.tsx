1c2c6051ded1cb37967ef27860b978044071c0a8
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2024-12-23 20:08:08 -0800

    Integrate Profile component into main app layout

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,10 +1,12 @@
 import Town from './Town';
 import { Route, Routes, Link } from 'react-router-dom';
+import Profile from './profile';
 
 export function App() {
   return (
     <div>
       <Town />
+      <Profile />
     </div>
   );
 }
