5a6aad11c5a231a4d75bfc6fa80c2b7f23cf75b4
Lalitowey <91431946+Lalitowey@users.noreply.github.com>
2024-12-23 20:07:38 -0800

    Implement Profile component for user character display

---
+++ client/src/app/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-avatar">PFP</div>
+      <span className="profile-name">Character Name</span>
+      <span className="profile-level">Level 1</span>
+    </div>
+  );
+};
+
+export default Profile;
