07c5e2d503891717ba36ebfe7f2c1ed7aaebcc30
Anirudh Ahuja <ani.ahuja219@gmail.com>
2025-02-20 23:31:16 -0800

    Backend now up and running

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://rpg-webapp.onrender.com';
 export const FDC_API_KEY = process.env.REACT_APP_FDC_API_KEY || 'HKQAe7aaouTd6XdviH4MHYtT8TKwv5O16axn1gq3';
