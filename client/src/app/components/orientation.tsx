98bde5d5f53b98a44fa622f02dd1f3cfd371b5ea
Anirudh Ahuja <ani.ahuja219@gmail.com>
2025-02-21 17:28:03 -0800

    Added logo to the orientation page

--- client/src/app/components/orientation.tsx
+++ client/src/app/components/orientation.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import logo from '../../assets/logo.png';
+import slime from '../../assets/slime.gif';
 
 const OrientationMessage: React.FC = () => {
   return (
     <div className="portrait-overlay">
-      <Image 
-        src="../../assets/slime.gif"
-        alt="Rotate device"
-        className="rotate-device-image"
-      />
-      <h2>Please Rotate Your Device</h2>
-      <p>Portrait mode is still under development. Please rotate your device to continue. Thank you!</p>
-    </div>
-  );
+        <Image src={logo} alt="Logo" className="rotate-device-logo" />
+        <Image src={slime} alt="Rotate device" className="rotate-device-image" />
+        <h2>Please Rotate Your Device</h2>
+        <p>Portrait mode is still under development. Please rotate your device to continue. Thank you!</p>
+        </div>
+    );
 };
 
 export default OrientationMessage; 
